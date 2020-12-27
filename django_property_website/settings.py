7af843053a0bc9fe3da035eec2d03929802a71dc
yashikas <yashikakhuranayashika@gmail.com>
2020-12-27 06:11:28 -0500

    Realtors and Listings App added

--- django_property_website/settings.py
+++ django_property_website/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings',
+   # 'realtors',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
