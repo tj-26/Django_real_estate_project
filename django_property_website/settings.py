0437cd7b4e0d49ea3f9181a8acc4d336872abd86
yashikas <yashikakhuranayashika@gmail.com>
2020-12-28 05:02:46 -0500

    Intial migrate completed

--- django_property_website/settings.py
+++ django_property_website/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'django_property_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'property',
+        'USER':'postgres',
+        'PASSWORD': 'yashika',
+        'HOST': 'localhost'
     }
 }
 
