44a0722a860d41e97fde17db14dd79b4f9aad839
yashikas <yashikakhuranayashika@gmail.com>
2021-01-04 04:51:44 -0500

    Data added to the listings page from the database

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .models import Listing
 
 # Create your views here.
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+    paginator =  Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {
+        'listings':paged_listings
+    }
 
-def listing(request):
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
